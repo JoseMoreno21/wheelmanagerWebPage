18720a57ccf994f460a93d116dd67b8d23421d71
JoseMoreno21 <57198383+JoseMoreno21@users.noreply.github.com>
2021-04-20 18:31:35 -0500

    Project creation
    The Wheel Manager Project creation

---
+++ src/main/java/com/example/wheelmanager/WheelmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.wheelmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheelmanagerApplication.class, args);
+    }
+
+}
